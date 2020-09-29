9ca1a81e0e9cd0e8bdb5dde5db92a8b992f363f1
Jesper Sandell (jesp143d) <jesp143d@zbc.dk>
2020-09-29 11:46:04 +0200

    Add project files.

---
+++ Core/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using Anemonis;
+using AutoMapper;
+using System.Text.Json;
+using Anemonis.RandomOrg;
+
+namespace Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+            var client = new RandomOrgClient("e6e6aa51-22d1-461d-9126-41761f9334f2");
+            string stop;
+
+
+            do
+            {
+                Console.WriteLine("Indtast det mindste tal: ");
+                int minTal = int.Parse(Console.ReadLine());
+                Console.WriteLine("Indtast det højste tal");
+                int maxTal = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Henter tal fra Random.org");
+                var randomTal = client.GenerateIntegersAsync(1, minTal,maxTal, false);
+
+                Console.WriteLine("Random tal: " + randomTal.Result.Random.Data[0]);
+
+                Console.WriteLine("Klik på enhver tast for at prøve igen eller klik på q for at stoppe");
+                stop = Console.ReadLine();
+            } while (stop.ToUpper() != "Q");
+        }
+    }
+}
